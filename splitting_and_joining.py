acc33adf883a4a38bd8776697fcf529a00e35011
Scott Schlangen <sschlangen@gmail.com>
2017-12-05 05:05:18 -0500

    Added split methods

---
+++ splitting_and_joining.py
@@ -0,0 +1,11 @@
+# Iterable means can be looped through
+
+# Split method by default breaks on white space
+
+print('hello there students'.split())
+
+# outputs 3 items into a list ['hello' , 'there' , 'students']
+
+# we can tell it what to break on
+print("red:blue:green".split(':'))
+
