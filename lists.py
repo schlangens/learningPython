979bca8b09de738d4ff6915db4ac92779f26a1a3
Scott Schlangen <sschlangen@gmail.com>
2017-12-05 04:55:01 -0500

    Adding different methods to add to lists

--- lists.py
+++ lists.py
@@ -7,9 +7,15 @@ my_list = [1, 2, 3, 4, 5, 6]
 
 my_list = my_list + [7, 8]
 
-# You can also use the append method like so
+# You can also use the append method like so This will add a single item to a list
 
 my_list.append(9)
+
+# another list method called extend. If you want to add multiple items.
+
+my_list.extend([10, 11, 12])
+
+#
 
 
 print(my_list)
