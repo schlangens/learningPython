dc46078c524bea64fd9f17289d3ef3a03f7bb115
Scott Schlangen <sschlangen@gmail.com>
2017-12-07 04:25:26 -0500

    added 2 function/function calls. Added notes to my txt document

---
+++ functions.py
@@ -0,0 +1,14 @@
+def hows_the_parrot():
+    print("He's pining for the fjords!")
+
+hows_the_parrot()
+
+
+def  lumberjack(name):
+    if name.lower() == 'kenneth':
+        print("Kenneth's a lumberjack, and he's okay!")
+    else:
+        print("{} sleeps all night and {} works all day".format(name, name))
+
+lumberjack("Kenneth")
+lumberjack("Bob")
